e688c0f68679c8ae0019218515be3294e3635a99
unknown <antonioribeiro626185@gmail.com>
2023-11-09 13:17:05 +0100

    primeira pagina

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+function Home() {
+    return (
+        <div>
+            <div>Good By </div>
+            <Contador />
+        </div>
+    )
+  }
+
+function Contador(){
+  const [contador, setContador] = useState(1);
+  function adicionarContador(){
+        setContador(contador + 1)
+  }
+  function diminuirContador(){
+      setContador(contador - 1)
+  }
+  return (
+    <div>
+      <div>{contador}</div>
+      <button onClick={adicionarContador}>Adicionar</button>
+      <button onClick={diminuirContador}>Diminuir</button>
+    </div>
+  )
+}
+
+export default Home
